9f225ab655cf4a552417588f3074a2829a876d40
lizanbajracharya <lizan.bajra97@gmail.com>
2019-12-02 11:24:39 +0545

    Login and dashboard activity

--- app/src/main/java/com/example/hamrobook_ebookstore/MainActivity.java
+++ app/src/main/java/com/example/hamrobook_ebookstore/MainActivity.java
@@ -2,13 +2,40 @@ package com.example.hamrobook_ebookstore;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText etUsername,etPassword;
+    Button btnLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin=findViewById(R.id.btnLogin);
+        etUsername=findViewById(R.id.etUsername);
+        etPassword=findViewById(R.id.etPassword);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username=etUsername.getText().toString();
+                String Password=etPassword.getText().toString();
+                if(username.equals("admin") && Password.equals("admin"))
+                {
+                    Intent intent=new Intent(MainActivity.this,DashboardActivity.class);
+                    startActivity(intent);
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Invalid Username or Password", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
