c3017fc104d3611dfd79e67cbde1017cb3288864
lizanbajracharya <lizan.bajra97@gmail.com>
2020-02-06 19:39:24 +0545

    design change

--- app/src/main/java/com/example/hamrobook_ebookstore/BookActivity.java
+++ app/src/main/java/com/example/hamrobook_ebookstore/BookActivity.java
@@ -1,14 +1,46 @@
 package com.example.hamrobook_ebookstore;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class BookActivity extends AppCompatActivity {
+    Toolbar toolbar;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.cart:
+                intent=new Intent(this, CartActivity.class);
+                intent.putExtra("name","cart");
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book);
+
+        toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
